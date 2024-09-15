33a967c8eb07eb768196cd0bf23cbc417dce9abf
Madanprakash07 <159546407+Madanprakash07@users.noreply.github.com>
2024-09-15 12:17:34 +0530

    Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -8,290 +8,307 @@ public class Calculator
 	public static void main(String[] args) 
 	{  
     		Frame f=new Frame("Maddy's Calculator"); 
-			f.setBackground(new Color(0, 0, 128));  
-
+		f.setBackground(new Color(0, 0, 128));  
+		Font font = new Font("Chiller", Font.BOLD, 20);  
 
     		TextField t1=new TextField("");  
     		t1.setBounds(20,50, 260,50); 
-			//t1.setBackground(Color.black);
-		
+		t1.setFont(font);  	
+
     		Button badd=new Button("+");  
     		badd.setBounds(216,150,60,30); 
-			badd.setBackground(new Color(128, 128, 128)); 
-  			badd.addActionListener(new ActionListener()
+		badd.setBackground(new Color(128, 128, 128)); 
+		badd.setFont(font);
+  		badd.addActionListener(new ActionListener()
+		{  
+			public void actionPerformed(ActionEvent e)
 			{  
-				public void actionPerformed(ActionEvent e)
-				{  
-					
-					a = Integer.parseInt(t1.getText());
-					operator = "+";
-					t1.setText("");			
-				}  
-			});  
+				a = Integer.parseInt(t1.getText());
+				operator = "+";
+				t1.setText("");			
+			}  
+		});  
 
 
-			Button bsub=new Button("-");  
+		Button bsub=new Button("-");  
     		bsub.setBounds(216,200,60,30); 
-			bsub.setBackground(new Color(128, 128, 128)); 
-  			bsub.addActionListener(new ActionListener()
-			{  
-				public void actionPerformed(ActionEvent e)
-				{  
-					
-					a = Integer.parseInt(t1.getText());
-					operator = "-";
-					t1.setText("");		
-								
-				}  
-			});  
+		bsub.setBackground(new Color(128, 128, 128)); 
+		bsub.setFont(font);
+  		bsub.addActionListener(new ActionListener()
+		{  
+			public void actionPerformed(ActionEvent e)
+			{  			
+				a = Integer.parseInt(t1.getText());
+				operator = "-";
+				t1.setText("");		
+			}  
+		});  
 
 
-			Button bmultiply=new Button("x");  
+		Button bmultiply=new Button("x");  
     		bmultiply.setBounds(216,250,60,30); 
-			bmultiply.setBackground(new Color(128, 128, 128)); 
-  			bmultiply.addActionListener(new ActionListener()
-			{  
-				public void actionPerformed(ActionEvent e)
-				{
-					
-					a = Integer.parseInt(t1.getText());
-					operator = "x";
-					t1.setText("");
-				}  
-			});
+		bmultiply.setBackground(new Color(128, 128, 128)); 
+		bmultiply.setFont(font);
+  		bmultiply.addActionListener(new ActionListener()
+		{  
+			public void actionPerformed(ActionEvent e)
+			{			
+				a = Integer.parseInt(t1.getText());
+				operator = "x";
+				t1.setText("");
+			}  
+		});
 
 
-			Button bdivide=new Button("/");  
+		Button bdivide=new Button("/");  
     		bdivide.setBounds(216,300,60,30); 
-			bdivide.setBackground(new Color(128, 128, 128)); 
-  			bdivide.addActionListener(new ActionListener()
-			{  
-				public void actionPerformed(ActionEvent e)
-				{  
-					
-					a = Integer.parseInt(t1.getText());
-					operator = "/";
-					t1.setText("");
-				}  
-			});
+		bdivide.setBackground(new Color(128, 128, 128)); 
+		bdivide.setFont(font);
+  		bdivide.addActionListener(new ActionListener()
+		{  
+			public void actionPerformed(ActionEvent e)
+			{  			
+				a = Integer.parseInt(t1.getText());
+				operator = "/";
+				t1.setText("");
+			}  
+		});
 
 
-			Button bequals=new Button("=");  
+		Button bequals=new Button("=");  
     		bequals.setBounds(152,300,60,30); 
-			bequals.setBackground(new Color(250, 0, 0)); 
-  			bequals.addActionListener(new ActionListener()
-			{  
-				public void actionPerformed(ActionEvent e)
+		bequals.setBackground(new Color(250, 0, 0)); 
+		bequals.setFont(font);
+		bequals.setForeground(Color.white);  
+  		bequals.addActionListener(new ActionListener()
+		{  
+			public void actionPerformed(ActionEvent e)
+			{
+				int b = Integer.parseInt(t1.getText());
+				double result = 0;
+				switch (operator) 
 				{
-					int b = Integer.parseInt(t1.getText());
-					int result = 0;
+					case "+":
+						result = a+b;
+						break;
+					case "-":
+						result = a-b;
+						break;
+					case "x":
+						result = a*b;
+						break;
+					case "/":
+						result = (double)a/b;
+						break;					
+					default:
+						break;
+				}
+				if (operator=="/")
+				{
+					t1.setText(String.valueOf(result));	
+				}
+				else
+				{
+					t1.setText(String.valueOf((int)result));
+				}
+					
 
-					switch (operator) 
-					{
-						case "+":
-							result = a+b;
-							break;
-						case "-":
-							result = a-b;
-							break;
-						case "x":
-							result = a*b;
-							break;
-						case "/":
-							result = a/b;
-							break;					
-						default:
-							break;
-					}
-					t1.setText(String.valueOf(result));
-
-				}  
-			});
+			}  
+		});
 
 
-			Button clear=new Button("CE");  
+		Button clear=new Button("CE");  
     		clear.setBounds(88,300,60,30); 
-			clear.setBackground(Color.darkGray); 
-  			clear.addActionListener(new ActionListener()
+		clear.setBackground(Color.darkGray); 
+		clear.setFont(font);
+		clear.setForeground(Color.white);
+  		clear.addActionListener(new ActionListener()
+		{  
+			public void actionPerformed(ActionEvent e)
 			{  
-				public void actionPerformed(ActionEvent e)
-				{  
-					t1.setText("0");
-				}  
-			});
+				t1.setText("");
+			}  
+		});
 
 
 
-			Button one=new Button("1");  
+		Button one=new Button("1");  
     		one.setBounds(24,150,60,30); 
-			one.setBackground(Color.lightGray); 
-  			one.addActionListener(new ActionListener()
+		one.setBackground(Color.lightGray); 
+		one.setFont(font);
+  		one.addActionListener(new ActionListener()
+		{  
+			public void actionPerformed(ActionEvent e)
 			{  
-				public void actionPerformed(ActionEvent e)
-				{  
-					String  exp = t1.getText();
-					exp += "1";
-					t1.setText(exp);
-				}  
-			});  
+				String  exp = t1.getText();
+				exp += "1";
+				t1.setText(exp);
+			}  
+		});  
 
 
-
-
-			Button two=new Button("2");  
+		Button two=new Button("2");  
     		two.setBounds(88,150,60,30); 
-			two.setBackground(Color.lightGray);
-  			two.addActionListener(new ActionListener()
+		two.setBackground(Color.lightGray);
+		two.setFont(font);
+  		two.addActionListener(new ActionListener()
+		{  
+			public void actionPerformed(ActionEvent e)
 			{  
-				public void actionPerformed(ActionEvent e)
-				{  
-					String  exp = t1.getText();
-					exp += "2";
-					t1.setText(exp);  
-				}  
-			});  
+				String  exp = t1.getText();
+				exp += "2";
+				t1.setText(exp);  
+			}  
+		});  
 
 
-			Button three=new Button("3");  
+		Button three=new Button("3");  
     		three.setBounds(152,150,60,30); 
-			three.setBackground(Color.lightGray);
-  			three.addActionListener(new ActionListener()
+		three.setBackground(Color.lightGray);
+		three.setFont(font);
+  		three.addActionListener(new ActionListener()
+		{  
+			public void actionPerformed(ActionEvent e)
 			{  
-				public void actionPerformed(ActionEvent e)
-				{  
-					String  exp = t1.getText();
-					exp += "3";
-					t1.setText(exp);  
-				}  
-			});  
+				String  exp = t1.getText();
+				exp += "3";
+				t1.setText(exp);  
+			}  
+		});  
 
 
-			Button four=new Button("4");  
+		Button four=new Button("4");  
     		four.setBounds(24,200,60,30); 
-			four.setBackground(Color.lightGray);
-  			four.addActionListener(new ActionListener()
+		four.setBackground(Color.lightGray);
+		four.setFont(font);
+  		four.addActionListener(new ActionListener()
+		{  
+			public void actionPerformed(ActionEvent e)
 			{  
-				public void actionPerformed(ActionEvent e)
-				{  
-					String  exp = t1.getText();
-					exp += "4";
-					t1.setText(exp);  
-				}  
-			});  
+				String  exp = t1.getText();
+				exp += "4";
+				t1.setText(exp);  
+			}  
+		});  
 
-
-			Button five=new Button("5");  
+		Button five=new Button("5");  
     		five.setBounds(88,200,60,30); 
-			five.setBackground(Color.lightGray);
-  			five.addActionListener(new ActionListener()
+		five.setBackground(Color.lightGray);
+		five.setFont(font);
+		five.addActionListener(new ActionListener()
+		{  
+			public void actionPerformed(ActionEvent e)
 			{  
-				public void actionPerformed(ActionEvent e)
-				{  
-					String  exp = t1.getText();
-					exp += "5";
-					t1.setText(exp);  
-				}  
-			});  
+				String  exp = t1.getText();
+				exp += "5";
+				t1.setText(exp);  
+			}  
+		});  
 
 
-			Button six=new Button("6");  
+		Button six=new Button("6");  
     		six.setBounds(152,200,60,30); 
-			six.setBackground(Color.lightGray);
-  			six.addActionListener(new ActionListener()
+		six.setBackground(Color.lightGray);
+		six.setFont(font);
+		six.addActionListener(new ActionListener()
+		{  
+			public void actionPerformed(ActionEvent e)
 			{  
-				public void actionPerformed(ActionEvent e)
-				{  
-					String  exp = t1.getText();
-					exp += "6";
-					t1.setText(exp);  
-				}  
-			});  
+				String  exp = t1.getText();
+				exp += "6";
+				t1.setText(exp);  
+			}  
+		});  
 
 
-			Button seven=new Button("7");  
+		Button seven=new Button("7");  
     		seven.setBounds(24,250,60,30); 
-			seven.setBackground(Color.lightGray);
-  			seven.addActionListener(new ActionListener()
+		seven.setBackground(Color.lightGray);
+		seven.setFont(font);
+		seven.addActionListener(new ActionListener()
+		{  
+			public void actionPerformed(ActionEvent e)
 			{  
-				public void actionPerformed(ActionEvent e)
-				{  
-					String  exp = t1.getText();
-					exp += "7";
-					t1.setText(exp);  
-				}  
-			});  
+				String  exp = t1.getText();
+				exp += "7";
+				t1.setText(exp);  
+			}  
+		});  
 
-
-			Button eight=new Button("8");  
+		Button eight=new Button("8");  
     		eight.setBounds(88,250,60,30); 
-			eight.setBackground(Color.lightGray);
-  			eight.addActionListener(new ActionListener()
+		eight.setBackground(Color.lightGray);
+		eight.setFont(font);
+		eight.addActionListener(new ActionListener()
+		{  
+			public void actionPerformed(ActionEvent e)
 			{  
-				public void actionPerformed(ActionEvent e)
-				{  
-					String  exp = t1.getText();
-					exp += "8";
-					t1.setText(exp);  
-				}  
-			});  
+				String  exp = t1.getText();
+				exp += "8";
+				t1.setText(exp);  
+			}  
+		});  
 
-
-			Button nine=new Button("9");  
+		Button nine=new Button("9");  
     		nine.setBounds(152,250,60,30); 
-			nine.setBackground(Color.lightGray);
-  			nine.addActionListener(new ActionListener()
+		nine.setBackground(Color.lightGray);
+		nine.setFont(font);
+		nine.addActionListener(new ActionListener()
+		{  
+			public void actionPerformed(ActionEvent e)
 			{  
-				public void actionPerformed(ActionEvent e)
-				{  
-					String  exp = t1.getText();
-					exp += "9";
-					t1.setText(exp);  
-				}  
-			});  
+				String  exp = t1.getText();
+				exp += "9";
+				t1.setText(exp);  
+			}  
+		});  
 
 
-			Button zero=new Button("0");  
+		Button zero=new Button("0");  
     		zero.setBounds(24,300,60,30); 
-			zero.setBackground(Color.lightGray);
-  			zero.addActionListener(new ActionListener()
+		zero.setBackground(Color.lightGray);
+		zero.setFont(font);
+		zero.addActionListener(new ActionListener()
+		{  
+			public void actionPerformed(ActionEvent e)
 			{  
-				public void actionPerformed(ActionEvent e)
-				{  
-					String  exp = t1.getText();
-					exp += "0";
-					t1.setText(exp); 
-				}  
-			});  
+				String  exp = t1.getText();
+				exp += "0";
+				t1.setText(exp); 
+			}  
+		});  
 
 
-
-
-
-
-
+		f.addWindowListener(new WindowAdapter() 
+		{
+			public void windowClosing(WindowEvent we) 
+			{
+				System.exit(0);  
+			}
+		});
+			
 
 
 	    	f.add(badd);
-			f.add(bsub);
-			f.add(bmultiply);
-			f.add(bdivide);
-			f.add(bequals);
-			f.add(clear);
-			f.add(one);
-			f.add(two);
-			f.add(three);
-			f.add(four);
-			f.add(five);
-			f.add(six);
-			f.add(seven);
-			f.add(eight);
-			f.add(nine);
-			f.add(zero);
-			f.add(t1);  
+		f.add(bsub);
+		f.add(bmultiply);
+		f.add(bdivide);
+		f.add(bequals);
+		f.add(clear);
+		f.add(one);
+		f.add(two);
+		f.add(three);
+		f.add(four);
+		f.add(five);
+		f.add(six);
+		f.add(seven);
+		f.add(eight);
+		f.add(nine);
+		f.add(zero);
+		f.add(t1);  
     		f.setSize(300,400);  
     		f.setLayout(null);  
     		f.setVisible(true);   
-//-----------------------------------------
 
 	}  
 }  
